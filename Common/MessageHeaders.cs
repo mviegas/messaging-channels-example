9c83b23a6349eed3c402271bae109880fe12dd70
Mateus Viegas <mateuscviegas@gmail.com>
2021-02-21 10:13:55 +0000

    Removed usings in MessageHeaders.cs

---
+++ Common/MessageHeaders.cs
@@ -0,0 +1,8 @@
+namespace MessagingChannels.Example.Common
+{
+    public static class MessageHeaders
+    {
+        public const string MessageId = "X-MESSAGE-ID";
+        public const string CorrelationId = "X-CORRELATION-ID";
+    }
+}
